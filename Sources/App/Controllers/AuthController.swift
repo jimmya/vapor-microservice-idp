bd1c27e6ee4164f6a792839ccefae1e40faf1ba6
Jimmy Arts <arts.jimmy@gmail.com>
2018-09-27 08:20:40 +0200

    Make route handler single level

--- Sources/App/Controllers/AuthController.swift
+++ Sources/App/Controllers/AuthController.swift
@@ -42,18 +42,18 @@ private extension AuthController {
         guard let refreshToken = getTokenRequest.refreshToken else { throw Abort(.badRequest) }
         let repository = try req.make(RefreshTokenRepository.self)
         let jwtConfig = try req.make(JWTConfig.self)
-        return repository.find(token: refreshToken).flatMap(to: GetTokenResponse.self) { token in
+        return repository.find(token: refreshToken).flatMap { (token) -> Future<RefreshToken> in
             guard let token = token else { throw Abort(.unauthorized) }
             guard token.expires.timeIntervalSinceNow > 0 else { throw Abort(.unauthorized) }
-            return repository.delete(token: token).flatMap(to: RefreshToken.self) {
+            return repository.delete(token: token).map { token }
+            }.flatMap(to: RefreshToken.self) { token in
                 let refreshToken = UUID().uuidString
                 let expires = Date(timeIntervalSinceNow: jwtConfig.refreshTokenValidDuration)
                 let refreshTokenEntity = RefreshToken(userID: token.userID, token: refreshToken, expires: expires)
                 return repository.store(token: refreshTokenEntity)
-                }.map { refreshToken in
-                    let accessToken = try jwtConfig.accessTokenForUserID(refreshToken.userID, issuerType: .password)
-                    return GetTokenResponse(accessToken: accessToken.token, refreshToken: refreshToken.token, expiresIn: accessToken.expiresIn)
-            }
+            }.map { refreshToken in
+                let accessToken = try jwtConfig.accessTokenForUserID(refreshToken.userID, issuerType: .password)
+                return GetTokenResponse(accessToken: accessToken.token, refreshToken: refreshToken.token, expiresIn: accessToken.expiresIn)
         }
     }
 }
